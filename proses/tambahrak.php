07eadf121a0001ee0435f3b3d2f5efe3ffeeeb3f
muhada12 <mdanil831193@gmail.com>
2026-07-31 06:56:03 +0700

    Membuat proses simpan data rak

--- proses/tambahrak.php
+++ proses/tambahrak.php
@@ -1,9 +1,81 @@
 <?php
 
-echo "<script>
+require_once "koneksi.php";
 
-alert('Proses simpan akan dibuat pada Commit 4.');
+if (!isset($_POST['nama_rak'])) {
 
-window.location='../index.php?page=tambahrak';
+    header("Location: ../index.php?page=tambahrak");
+    exit;
 
-</script>";
+}
+
+$nama_rak = trim(mysqli_real_escape_string($koneksi, $_POST['nama_rak']));
+
+if ($nama_rak == "") {
+
+    echo "<script>
+
+            alert('Nama rak tidak boleh kosong.');
+
+            window.location='../index.php?page=tambahrak';
+
+          </script>";
+
+    exit;
+
+}
+
+$cek = mysqli_query($koneksi, "
+    SELECT id
+    FROM rak
+    WHERE nama_rak='$nama_rak'
+");
+
+if (mysqli_num_rows($cek) > 0) {
+
+    echo "<script>
+
+            alert('Nama rak sudah digunakan.');
+
+            window.location='../index.php?page=tambahrak';
+
+          </script>";
+
+    exit;
+
+}
+
+$simpan = mysqli_query($koneksi, "
+    INSERT INTO rak
+    (
+        nama_rak,
+        created_at
+    )
+    VALUES
+    (
+        '$nama_rak',
+        NOW()
+    )
+");
+
+if ($simpan) {
+
+    echo "<script>
+
+            alert('Data rak berhasil ditambahkan.');
+
+            window.location='../index.php?page=datarak';
+
+          </script>";
+
+} else {
+
+    echo "<script>
+
+            alert('Data rak gagal disimpan.');
+
+            window.location='../index.php?page=tambahrak';
+
+          </script>";
+
+}
