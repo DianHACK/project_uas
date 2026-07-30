e14b7af4c1765b5a1569498225a33c711181d5e1
TriWidiantoko <50510417+DianHACK@users.noreply.github.com>
2026-07-31 02:39:21 +0700

    Delete pages directory

--- pages/home.php
+++
@@ -1,6 +0,0 @@
-<div class="container-fluid" style="margin-bottom: 65vh">
-    <div class="text-center">
-        <h1><b>Selamat datang, <?= ucwords($_SESSION['username']) ?> di Super Market kami</b></h1>
-
-    </div>
-</div>
