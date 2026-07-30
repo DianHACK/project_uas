103d1718fe9388643ce6a7f352d662e7443ba18c
Tri Widiantoko <triwidiantoko@gmail.com>
2026-07-31 03:21:31 +0700

    Menyiapkan struktur awal project dan template aplikasi

---
+++ komponen/footer.php
@@ -0,0 +1,13 @@
+<footer>
+        <div class="container-fluid">
+          <div class="d-flex justify-content-between py-7 flex-md-nowrap flex-wrap gap-sm-0 gap-3">
+            <div class="d-flex gap-3 align-items-center">
+              <img src="assets/images/logos/favicon.png" alt="icon">
+              <p class="fs-4 mb-0">All rights reserved by Modernize. </p>
+            </div>
+            <div>
+              <p class="mb-0">Produced by <a target="_blank" href="https://adminmart.com/" class="text-primary link-primary">AdminMart</a>.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
